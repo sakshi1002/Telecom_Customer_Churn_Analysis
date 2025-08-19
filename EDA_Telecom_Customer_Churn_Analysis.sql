673488791caa49cb1cc9ca58446c1932b8de9060
Sakshi Gaikwad <64495552+sakshi1002@users.noreply.github.com>
2025-08-19 21:17:41 +0530

    Add files via upload
    Uploaded All files

---
+++ EDA_Telecom_Customer_Churn_Analysis.sql
@@ -0,0 +1,158 @@
+--Gender wise analysis
+
+SELECT * FROM stg_Churn
+
+with total_customers as (
+	SELECT COUNT(customer_id) as total_cnt
+	FROM stg_Churn
+)
+SELECT Gender, COUNT(Gender) AS CountCustomer, ROUND(100.0*(CAST(COUNT(Gender) AS FLOAT)/tc.total_cnt),2) as percentcount
+FROM stg_Churn
+CROSS JOIN total_customers tc
+GROUP BY Gender,tc.total_cnt
+
+
+--Contract type Analysis
+
+SELECT Contract, COUNT(Contract) as TotalCnt,
+ROUND(100.0*(CAST(COUNT(Contract) AS float)/(SELECT COUNT(*) FROM stg_Churn)),2) as Percentage
+FROM stg_Churn
+GROUP BY Contract
+
+--Customer Status Analysis
+
+SELECT Customer_Status,Churn_Category,
+count(*) as cnt
+FROM stg_Churn
+GROUP BY Customer_Status,Churn_Category
+
+
+
+
+SELECT Customer_Status, Count(Customer_Status) as TotalCnt, SUM(total_Revenue) as TotalRev,
+SUM(Total_Revenue) / (Select SUM(Total_Revenue) FROM stg_Churn) * 100 as RevPercentage
+FROM stg_Churn
+GROUP BY Customer_Status
+
+
+--State wise analysis
+
+SELECT State,Customer_Status, COUNT(*) as tot_cnt
+FROM stg_Churn
+GROUP BY State,Customer_Status
+ORDER BY tot_cnt DESC
+
+
+SELECT State, Count(State) as TotalCnt, SUM(total_Revenue) as TotalRev,
+SUM(Total_Revenue) / (Select SUM(Total_Revenue) FROM stg_Churn) * 100 as RevPercentage
+FROM stg_Churn
+GROUP BY State
+ORDER BY RevPercentage DESC
+
+
+SELECT DISTINCT(Internet_Type)
+FROM stg_Churn
+
+--DELETE FROM stg_Churn
+--WHERE Internet_Type IS Null;
+
+--DATA CLEANING 
+
+-- Checking null values in the table
+
+SELECT 
+    SUM(CASE WHEN Customer_ID IS NULL THEN 1 ELSE 0 END) AS Customer_ID_Null_Count,
+    SUM(CASE WHEN Gender IS NULL THEN 1 ELSE 0 END) AS Gender_Null_Count,
+    SUM(CASE WHEN Age IS NULL THEN 1 ELSE 0 END) AS Age_Null_Count,
+    SUM(CASE WHEN Married IS NULL THEN 1 ELSE 0 END) AS Married_Null_Count,
+    SUM(CASE WHEN State IS NULL THEN 1 ELSE 0 END) AS State_Null_Count,
+    SUM(CASE WHEN Number_of_Referrals IS NULL THEN 1 ELSE 0 END) AS Number_of_Referrals_Null_Count,
+    SUM(CASE WHEN Tenure_in_Months IS NULL THEN 1 ELSE 0 END) AS Tenure_in_Months_Null_Count,
+    SUM(CASE WHEN Value_Deal IS NULL THEN 1 ELSE 0 END) AS Value_Deal_Null_Count,
+    SUM(CASE WHEN Phone_Service IS NULL THEN 1 ELSE 0 END) AS Phone_Service_Null_Count,
+    SUM(CASE WHEN Multiple_Lines IS NULL THEN 1 ELSE 0 END) AS Multiple_Lines_Null_Count,
+    SUM(CASE WHEN Internet_Service IS NULL THEN 1 ELSE 0 END) AS Internet_Service_Null_Count,
+    SUM(CASE WHEN Internet_Type IS NULL THEN 1 ELSE 0 END) AS Internet_Type_Null_Count,
+    SUM(CASE WHEN Online_Security IS NULL THEN 1 ELSE 0 END) AS Online_Security_Null_Count,
+    SUM(CASE WHEN Online_Backup IS NULL THEN 1 ELSE 0 END) AS Online_Backup_Null_Count,
+    SUM(CASE WHEN Device_Protection_Plan IS NULL THEN 1 ELSE 0 END) AS Device_Protection_Plan_Null_Count,
+    SUM(CASE WHEN Premium_Support IS NULL THEN 1 ELSE 0 END) AS Premium_Support_Null_Count,
+    SUM(CASE WHEN Streaming_TV IS NULL THEN 1 ELSE 0 END) AS Streaming_TV_Null_Count,
+    SUM(CASE WHEN Streaming_Movies IS NULL THEN 1 ELSE 0 END) AS Streaming_Movies_Null_Count,
+    SUM(CASE WHEN Streaming_Music IS NULL THEN 1 ELSE 0 END) AS Streaming_Music_Null_Count,
+    SUM(CASE WHEN Unlimited_Data IS NULL THEN 1 ELSE 0 END) AS Unlimited_Data_Null_Count,
+    SUM(CASE WHEN Contract IS NULL THEN 1 ELSE 0 END) AS Contract_Null_Count,
+    SUM(CASE WHEN Paperless_Billing IS NULL THEN 1 ELSE 0 END) AS Paperless_Billing_Null_Count,
+    SUM(CASE WHEN Payment_Method IS NULL THEN 1 ELSE 0 END) AS Payment_Method_Null_Count,
+    SUM(CASE WHEN Monthly_Charge IS NULL THEN 1 ELSE 0 END) AS Monthly_Charge_Null_Count,
+    SUM(CASE WHEN Total_Charges IS NULL THEN 1 ELSE 0 END) AS Total_Charges_Null_Count,
+    SUM(CASE WHEN Total_Refunds IS NULL THEN 1 ELSE 0 END) AS Total_Refunds_Null_Count,
+    SUM(CASE WHEN Total_Extra_Data_Charges IS NULL THEN 1 ELSE 0 END) AS Total_Extra_Data_Charges_Null_Count,
+    SUM(CASE WHEN Total_Long_Distance_Charges IS NULL THEN 1 ELSE 0 END) AS Total_Long_Distance_Charges_Null_Count,
+    SUM(CASE WHEN Total_Revenue IS NULL THEN 1 ELSE 0 END) AS Total_Revenue_Null_Count,
+    SUM(CASE WHEN Customer_Status IS NULL THEN 1 ELSE 0 END) AS Customer_Status_Null_Count,
+    SUM(CASE WHEN Churn_Category IS NULL THEN 1 ELSE 0 END) AS Churn_Category_Null_Count,
+    SUM(CASE WHEN Churn_Reason IS NULL THEN 1 ELSE 0 END) AS Churn_Reason_Null_Count
+FROM stg_Churn;
+
+
+--Remove null and insert the new data into Prod table
+
+SELECT 
+    Customer_ID,
+    Gender,
+    Age,
+    Married,
+    State,
+    Number_of_Referrals,
+    Tenure_in_Months,
+    ISNULL(Value_Deal, 'None') AS Value_Deal,
+    Phone_Service,
+    ISNULL(Multiple_Lines, 'No') As Multiple_Lines,
+    Internet_Service,
+    ISNULL(Internet_Type, 'None') AS Internet_Type,
+    ISNULL(Online_Security, 'No') AS Online_Security,
+    ISNULL(Online_Backup, 'No') AS Online_Backup,
+    ISNULL(Device_Protection_Plan, 'No') AS Device_Protection_Plan,
+    ISNULL(Premium_Support, 'No') AS Premium_Support,
+    ISNULL(Streaming_TV, 'No') AS Streaming_TV,
+    ISNULL(Streaming_Movies, 'No') AS Streaming_Movies,
+    ISNULL(Streaming_Music, 'No') AS Streaming_Music,
+    ISNULL(Unlimited_Data, 'No') AS Unlimited_Data,
+    Contract,
+    Paperless_Billing,
+    Payment_Method,
+    Monthly_Charge,
+    Total_Charges,
+    Total_Refunds,
+    Total_Extra_Data_Charges,
+    Total_Long_Distance_Charges,
+    Total_Revenue,
+    Customer_Status,
+    ISNULL(Churn_Category, 'Others') AS Churn_Category,
+    ISNULL(Churn_Reason , 'Others') AS Churn_Reason
+
+INTO [db_Churn].[dbo].[prod_Churn]
+FROM [db_Churn].[dbo].[stg_Churn];
+
+
+SELECT TOP 10 * FROM prod_Churn
+
+--CREATING VIEWS
+
+CREATE VIEW vw_ChurData AS 
+	SELECT * 
+	FROM prod_Churn
+	WHERE Customer_Status IN ('Churned','Stayed')
+
+
+CREATE VIEW vw_JoinData AS
+	SELECT * 
+	FROM prod_Churn
+	WHERE Customer_Status = 'Joined'
+
+----------------
+
+SELECT * FROM dbo.vw_ChurData
+
+SELECT * FROM dbo.vw_JoinData
